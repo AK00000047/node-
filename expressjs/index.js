12a695c7caafa720ad90a28123cd47df038f4a71
Amit Kumar <amit.kumar@kainskep.com>
2023-07-20 17:23:25 +0530

    this is node js appli

---
+++ expressjs/index.js
@@ -0,0 +1,24 @@
+const fs=require('fs');
+const express=require('express');
+const path=require('path');
+const server=express();
+
+
+const insta =fs.readFileSync('index.html','utf-8');
+const apple=fs.readFileSync('apple.html','utf-8');
+// const auth=(req,res,next)=>{
+//     console.log(req.query);
+//     if(req.query.password){
+//         //res.send("autenticate");
+//         next();
+//     }else {
+//         res.send('404 error');
+//     }
+// }
+server.get('/insta',(req,res)=>{
+   res.send(insta);
+});
+server.get('/insta/products',(req,res)=>{
+    res.send(apple);
+})
+server.listen(399);
